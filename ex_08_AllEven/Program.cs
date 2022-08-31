3f2c0c0737169d7319a0819ac89b7e534b52c76c
Tatiana Kalinina <tankakalinka@gmail.com>
2022-08-31 15:56:26 +0300

    add hometask 8 AllEvenFrom[1,N]

---
+++ ex_08_AllEven/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Input any integer number: ");
+string number = Console.ReadLine();
+int N = int.Parse(number);
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i}, ");
+    }
+}
