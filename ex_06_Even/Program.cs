76cd3735dbe1e7e4d531e4693b0fd7448203073c
Tatiana Kalinina <tankakalinka@gmail.com>
2022-08-31 15:42:28 +0300

    add hometask 6 EvenNumber

---
+++ ex_06_Even/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(-100, 100);
+Console.WriteLine(number);
+// Console.Write("number = ");
+// string numberN = Console.ReadLine();
+// int number = int.Parse(numberN);
+if (number % 2 == 0)
+{
+    Console.WriteLine("yes");
+}
+else 
+{
+    Console.WriteLine("no");
+}
