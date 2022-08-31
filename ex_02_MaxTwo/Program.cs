8858b4bf4ed86867e468a8ab71fb48bfda83b9e0
Tatiana Kalinina <tankakalinka@gmail.com>
2022-08-31 14:34:17 +0300

    add hometask 2 MaxFromTwo

---
+++ ex_02_MaxTwo/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("a = ");
+string inputa = Console.ReadLine();
+int a = int.Parse(inputa);
+Console.Write("b = ");
+string inputb = Console.ReadLine();
+int b = int.Parse(inputb);
+if (a > b)
+{
+    Console.Write($"min = {b} ");
+    Console.WriteLine($"max = {a}");
+}
+else if (b > a)
+{
+    Console.Write($"min = {a} ");
+    Console.WriteLine($"max = {b}");
+}
