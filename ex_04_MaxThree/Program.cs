c47d09ef02febf5e900acc4f87b5deda6a36ac3f
Tatiana Kalinina <tankakalinka@gmail.com>
2022-08-31 15:32:09 +0300

    add hometask 4 MaxFromThree

---
+++ ex_04_MaxThree/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("a = ");
+string inputa = Console.ReadLine();
+int a = int.Parse(inputa);
+Console.Write("b = ");
+string inputb = Console.ReadLine();
+int b = int.Parse(inputb);
+Console.Write("c = ");
+string inputc = Console.ReadLine();
+int c = int.Parse(inputc);
+// int a = new Random().Next(0, 100);
+// Console.WriteLine(a);
+// int b = new Random().Next(0, 100);
+// Console.WriteLine(b);
+// int c = new Random().Next(0, 100);
+// Console.WriteLine(c);
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine($"max = {max}");
